3abee4b460e80ae1b489921f05c071d6af2621c0
João Gabriel <joaogabrieloliveira9@gmail.com>
2021-08-05 22:44:00 -0300

    Modelo de usuário mínimo

--- superlists/settings.py
+++ superlists/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Auth
+
+AUTH_USER_MODEL = 'accounts.User'
